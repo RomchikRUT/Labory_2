846a44ad6286b81d252efd101d81483f396b3825
RomchikRUT <1324overlord1324@gmail.com>
2024-11-19 15:36:03 +0300

    second changes

--- src/main/java/Labory2/Main.java
+++ src/main/java/Labory2/Main.java
@@ -1,7 +1,37 @@
 package Labory2;
 
+import java.lang.*;
+import java.util.Objects;
+import java.util.Scanner;
+import java.lang.String;
+
+/**
+ * Main class
+ */
 public class Main {
+    /**
+     * Main method
+     * @param args main arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        boolean isCalculated;
+        String marker = "start";
+        Scanner input = new Scanner(System.in);
+        while (!Objects.equals(marker, "exit")){
+            System.out.print("Enter the expression: ");
+            String str = input.nextLine();
+
+            Calculator expression = new Calculator(str);
+            isCalculated = expression.calculate();
+
+            if (!isCalculated) {
+                System.out.print("Input is incorrect.");
+            } else {
+                System.out.print(str + " = ");
+                System.out.println(expression.getAnswer().toString());
+            }
+            System.out.println("Type 'exit' to end program.");
+            marker = input.nextLine();
+        }
     }
 }
